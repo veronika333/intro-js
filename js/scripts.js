faa4131ba378e98384f8cc843308acae35439d20
Veronika <vera3@inbox.ru>
2020-01-31 09:35:34 +0200

    q-query-added, doesnt work

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
+$(document).ready(function() {
+  $(".slider__button--next").on("click", getNext);
+  $(".slider__button--prev").on("click", getNext);
+});
+
 (function() {
   var itemClassName = "slider__photo";
   (items = document.getElementsByClassName(itemClassName)),
     (totalItems = items.length),
     (slide = 0);
 
+  /*  
   // Set event listeners
   function setEventListeners() {
     var next = document.getElementsByClassName("slider__button--next")[0],
@@ -11,8 +17,8 @@
 
     next.addEventListener("click", getNext);
     prev.addEventListener("click", getPrev);
-  }
-
+  } */
+  /*
   function getNext() {
     // TODO
     items[slide].classList.remove("active");
@@ -23,16 +29,35 @@
     }
     items[slide].classList.add("active");
   }
+*/
+  function getNext() {
+    // TODO
+    $(".slider__photo")
+      .eq(0)
+      .removeClass("active");
+    if (slide === totalItems - 1) {
+      slide = 0;
+    } else {
+      slide++;
+    }
+    $(".slider__photo")
+      .eq(0)
+      .addClass("active");
+  }
 
   function getPrev() {
     // TODO
-    items[slide].classList.remove("active");
+    $(".slider__photo")
+      .eq(0)
+      .removeClass("active");
     if (slide === 0) {
       slide = items.length - 1;
     } else {
       slide--;
     }
-    items[slide].classList.add("active");
+    $(".slider__photo")
+      .eq(0)
+      .addClass("active");
   }
 
   function initSlider() {
